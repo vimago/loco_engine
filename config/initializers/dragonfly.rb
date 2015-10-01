de3971eed1a6dd441832c32155047c372c860fab
Victor Gomez <victor@bluerocket.us>
2015-10-01 09:39:06 -0600

    Initial Commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'baac576cc2dc3bcd3cdf60803c494affbc5526ce79ca0ce281a4ca4002a60295'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coh::Application.config.secret_token = 'f3141a227af2ee952072358b6434646d2141e018ba3c3f0c2b200bf4c83e057463265eaab4c84ccd6b102294ae65dccb1f459fe4371536f02c6c1d1b00fa67d6'
